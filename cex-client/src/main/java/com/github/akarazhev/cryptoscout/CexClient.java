939500896af0cb8fd9e654790a96f22a0936fe32
Andrey Karazhev <karazhev@gmail.com>
2025-02-03 14:42:26 +0300

    Added the basic cex client application.

---
+++ cex-client/src/main/java/com/github/akarazhev/cryptoscout/CexClient.java
@@ -0,0 +1,13 @@
+package com.github.akarazhev.cryptoscout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CexClient {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CexClient.class, args);
+	}
+
+}
