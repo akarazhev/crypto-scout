de8bf87611b6879ab782f0833651fe98850533f5
Andrey Karazhev <karazhev@gmail.com>
2025-02-11 14:03:26 +0300

    Added a configuration for amqp.

---
+++ event-collector/src/main/java/com/github/akarazhev/cryptoscout/config/AmqpConfig.java
@@ -0,0 +1,16 @@
+package com.github.akarazhev.cryptoscout.config;
+
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class AmqpConfig {
+
+    @Bean
+    public TopicExchange announcementsTopicExchange(@Value("${amqp.exchange.announcements}") final String name) {
+        return ExchangeBuilder.topicExchange(name).durable(true).build();
+    }
+}
