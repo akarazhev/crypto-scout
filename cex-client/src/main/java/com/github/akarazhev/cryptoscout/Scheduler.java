3b3e3f44842a4c061d13d60815fc48d7fe2593c8
Andrey Karazhev <karazhev@gmail.com>
2025-02-13 09:49:03 +0300

    Update the scheduler class to be more testable.

--- cex-client/src/main/java/com/github/akarazhev/cryptoscout/Scheduler.java
+++ cex-client/src/main/java/com/github/akarazhev/cryptoscout/Scheduler.java
@@ -2,7 +2,6 @@ package com.github.akarazhev.cryptoscout;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -10,17 +9,19 @@ import org.springframework.stereotype.Service;
 @Service
 final class Scheduler {
     private static final Logger LOGGER = LoggerFactory.getLogger(Scheduler.class);
-    @Autowired
-    @Qualifier("bybitEventSource")
-    private EventSource bybitEventSource;
-    @Autowired
-    private EventExchange eventExchangeService;
+    private final EventSource bybitEventSource;
+    private final EventExchange eventExchange;
+
+    public Scheduler(@Qualifier("bybitEventSource") final EventSource bybitEventSource, final EventExchange eventExchange) {
+        this.bybitEventSource = bybitEventSource;
+        this.eventExchange = eventExchange;
+    }
 
     @Scheduled(fixedRate = 60000) // Runs every 60 seconds
     public void perform() {
         LOGGER.info("Running scheduled task at {}", new java.util.Date());
         bybitEventSource.getEvents().forEach(e -> {
-            eventExchangeService.publish(e);
+            eventExchange.publish(e);
             LOGGER.info(e.toString());
         });
     }
