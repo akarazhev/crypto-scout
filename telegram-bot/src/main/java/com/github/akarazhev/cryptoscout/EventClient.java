404ae74ed284fc44d5a6dd9517651ac7d57ec0a9
Andrey Karazhev <karazhev@gmail.com>
2025-02-26 17:09:45 +0300

    Added a basic project structure for a bot.

---
+++ telegram-bot/src/main/java/com/github/akarazhev/cryptoscout/EventClient.java
@@ -0,0 +1,61 @@
+package com.github.akarazhev.cryptoscout;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.longpolling.BotSession;
+import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
+import org.telegram.telegrambots.longpolling.starter.AfterBotRegistration;
+import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+@Service
+class EventClient implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
+    private final TelegramClient telegramClient;
+    private final String telegramBotToken;
+
+    public EventClient(@Value("${telegram.bot.token}") String telegramBotToken) {
+        this.telegramBotToken = telegramBotToken;
+        this.telegramClient = new OkHttpTelegramClient(getBotToken());
+    }
+
+    @Override
+    public String getBotToken() {
+        return telegramBotToken;
+    }
+
+    @Override
+    public LongPollingUpdateConsumer getUpdatesConsumer() {
+        return this;
+    }
+
+    @Override
+    public void consume(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = SendMessage // Create a message object
+                    .builder()
+                    .chatId(chat_id)
+                    .text(message_text)
+                    .build();
+            try {
+                telegramClient.execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @AfterBotRegistration
+    public void afterRegistration(BotSession botSession) {
+        System.out.println("Registered bot running state is: " + botSession.isRunning());
+    }
+}
---
+++ telegram-bot/src/main/java/com/github/akarazhev/cryptoscout/TelegramBot.java
@@ -0,0 +1,12 @@
+package com.github.akarazhev.cryptoscout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBot.class, args);
+    }
+}
