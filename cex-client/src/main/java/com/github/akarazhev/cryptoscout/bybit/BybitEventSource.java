aed82474d19230b92a5c73d89dc38a78bf44fda0
Andrey Karazhev <karazhev@gmail.com>
2025-03-05 16:24:08 +0300

    Parse tags and types from the config.

--- cex-client/src/main/java/com/github/akarazhev/cryptoscout/bybit/BybitEventSource.java
+++ cex-client/src/main/java/com/github/akarazhev/cryptoscout/bybit/BybitEventSource.java
@@ -22,6 +22,8 @@ final class BybitEventSource implements EventSource {
     private final RestTemplate restTemplate;
     private final String url;
     private final String locale;
+    private final String[] tags;
+    private final String[] types;
     private final List<AnnouncementFetcher> fetchers;
 
     public BybitEventSource(final RestTemplateBuilder builder,
@@ -32,7 +34,9 @@ final class BybitEventSource implements EventSource {
         this.restTemplate = builder.build();
         this.url = url.trim();
         this.locale = locale.trim();
-        this.fetchers = createFetchers(tags.trim().split(","), types.trim().split(","));
+        this.tags = valueAsArray(tags);
+        this.types = valueAsArray(types);
+        this.fetchers = createFetchers(this.tags, this.types);
         LOGGER.info("Initializing BybitEventSource with locale: '{}', types: '{}', tags: '{}'", locale, types, tags);
     }
 
@@ -63,6 +67,28 @@ final class BybitEventSource implements EventSource {
     }
 
     private Event getEvent(final Announcement announcement) {
-        return new Event(Event.Platform.BYBIT, announcement.type().key(), announcement);
+        return new Event(Event.Platform.BYBIT, announcement.dateTimestamp(), getType(announcement), announcement.title(),
+                announcement.description(), announcement.url());
+    }
+
+    private String[] valueAsArray(final String value) {
+        final var trimmedValue = value.trim();
+        return !trimmedValue.isEmpty() ? trimmedValue.split(",") : new String[0];
+    }
+
+    private String getType(final Announcement announcement) {
+        for (final var tag : tags) {
+            if (announcement.tags().contains(tag)) {
+                return tag;
+            }
+        }
+
+        for (final var type : types) {
+            if (type.equals(announcement.type().key())) {
+                return type;
+            }
+        }
+
+        return "Unknown";
     }
 }
