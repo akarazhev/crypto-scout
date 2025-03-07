81f133293840d4d14198885c051619a679160d1d
Andrey Karazhev <karazhev@gmail.com>
2025-03-07 23:54:45 +0300

    Added configs for topics

--- cex-client/src/main/java/com/github/akarazhev/cryptoscout/config/AmqpConfig.java
+++ cex-client/src/main/java/com/github/akarazhev/cryptoscout/config/AmqpConfig.java
@@ -3,6 +3,7 @@ package com.github.akarazhev.cryptoscout.config;
 import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +12,20 @@ import org.springframework.context.annotation.Configuration;
 class AmqpConfig {
 
     @Bean
+    @Qualifier("announcementsExchange")
     public TopicExchange announcementsTopicExchange(@Value("${amqp.exchange.announcements}") final String name) {
+        return ExchangeBuilder.topicExchange(name).durable(true).build();
+    }
+
+    @Bean
+    @Qualifier("commandsExchange")
+    public TopicExchange commandsTopicExchange(@Value("${amqp.exchange.commands}") final String name) {
+        return ExchangeBuilder.topicExchange(name).durable(true).build();
+    }
+
+    @Bean
+    @Qualifier("resultsExchange")
+    public TopicExchange resultsTopicExchange(@Value("${amqp.exchange.results}") final String name) {
         return ExchangeBuilder.topicExchange(name).durable(true).build();
     }
 
