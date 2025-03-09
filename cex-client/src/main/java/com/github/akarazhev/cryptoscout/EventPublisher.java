b3964450d3b4eb5ec9cc64e46f2511b6d1e296df
Andrey Karazhev <karazhev@gmail.com>
2025-03-09 18:23:22 +0300

    Refactored services

--- cex-client/src/main/java/com/github/akarazhev/cryptoscout/EventPublisher.java
+++ cex-client/src/main/java/com/github/akarazhev/cryptoscout/EventPublisher.java
@@ -7,11 +7,11 @@ import org.springframework.stereotype.Service;
 import static com.github.akarazhev.cryptoscout.Constants.AMQP_ROUTING_ANNOUNCEMENTS;
 
 @Service
-final class PublisherService implements Publisher<Event> {
+final class EventPublisher implements Publisher<Event> {
     private final AmqpTemplate amqpTemplate;
     private final String exchange;
 
-    public PublisherService(final AmqpTemplate amqpTemplate, @Value("${amqp.exchange.announcements}") final String exchange) {
+    public EventPublisher(final AmqpTemplate amqpTemplate, @Value("${amqp.exchange.announcements}") final String exchange) {
         this.amqpTemplate = amqpTemplate;
         this.exchange = exchange;
     }
