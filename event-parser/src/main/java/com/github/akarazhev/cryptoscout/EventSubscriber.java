0d9402fd919bb19ad66385b86e3b35e79af3d3a2
Andrey Karazhev <andrey.karazhev@specific-group.com>
2025-08-07 16:55:17 +0300

    Added logger

--- event-parser/src/main/java/com/github/akarazhev/cryptoscout/EventSubscriber.java
+++ event-parser/src/main/java/com/github/akarazhev/cryptoscout/EventSubscriber.java
@@ -24,16 +24,18 @@
 
 package com.github.akarazhev.cryptoscout;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 final class EventSubscriber implements Subscriber<String> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventSubscriber.class);
 
     @RabbitListener(queues = "${amqp.queue.commands}")
     @Override
     public void subscribe(final String event) {
-        System.out.println("Received event: " + event);
-        // process the message here
+        LOGGER.info("Received event: {}", event);
     }
 }
