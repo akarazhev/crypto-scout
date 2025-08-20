0a4a31638617f872cee795fbd8afcf9fe8d7a763
Andrey Karazhev <andrey.karazhev@specific-group.com>
2025-08-20 12:02:47 +0300

    Renamed the module: cmc-client -> crypto-scout-client

--- crypto-scout-client/src/main/java/com/github/akarazhev/cryptoscout/CryptoScoutClient.java
+++ crypto-scout-client/src/main/java/com/github/akarazhev/cryptoscout/CryptoScoutClient.java
@@ -28,9 +28,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CmcClient {
+public class CryptoScoutClient {
 
     public static void main(final String[] args) {
-        SpringApplication.run(CmcClient.class, args);
+        SpringApplication.run(CryptoScoutClient.class, args);
     }
 }
