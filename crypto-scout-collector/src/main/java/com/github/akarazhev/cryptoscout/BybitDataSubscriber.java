517b483997da33c7c3b6f234e9f9edb42f2d4e2f
Andrey Karazhev <andrey.karazhev@specific-group.com>
2025-08-27 21:40:17 +0300

    Fixed consuming the data

--- crypto-scout-collector/src/main/java/com/github/akarazhev/cryptoscout/BybitDataSubscriber.java
+++ crypto-scout-collector/src/main/java/com/github/akarazhev/cryptoscout/BybitDataSubscriber.java
@@ -25,25 +25,26 @@
 package com.github.akarazhev.cryptoscout;
 
 import com.github.akarazhev.cryptoscout.bybit.BybitService;
+import com.github.akarazhev.jcryptolib.stream.Payload;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
-final class EventSubscriber implements Subscriber<Event> {
-    private static final Logger LOGGER = LoggerFactory.getLogger(EventSubscriber.class);
+final class BybitDataSubscriber implements Subscriber<Payload<Map<String, Object>>> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BybitDataSubscriber.class);
     private final BybitService bybitService;
 
-    public EventSubscriber(final BybitService bybitService) {
+    public BybitDataSubscriber(final BybitService bybitService) {
         this.bybitService = bybitService;
     }
 
-    @RabbitListener(queues = "${amqp.queue.announcements}")
+    @RabbitListener(queues = "${amqp.queue.bybit}")
     @Override
-    public void subscribe(final Event event) {
-        if (Event.Platform.BYBIT.equals(event.platform())) {
-            bybitService.save(event);
-        }
+    public void subscribe(final Payload<Map<String, Object>> payload) {
+        LOGGER.info("Received payload: {}", payload);
     }
 }
--- crypto-scout-collector/src/main/java/com/github/akarazhev/cryptoscout/config/AmqpConfig.java
+++ crypto-scout-collector/src/main/java/com/github/akarazhev/cryptoscout/config/AmqpConfig.java
@@ -24,28 +24,31 @@
 
 package com.github.akarazhev.cryptoscout.config;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
-import org.springframework.messaging.handler.annotation.support.MessageHandlerMethodFactory;
 
 import java.time.Duration;
 
 import static com.github.akarazhev.cryptoscout.Constants.AMQP.ROUTING_ANNOUNCEMENTS;
 import static com.github.akarazhev.cryptoscout.Constants.AMQP.ROUTING_COMMANDS;
+import static com.github.akarazhev.cryptoscout.Constants.AMQP.ROUTING_KEY_CLIENT;
+import static com.github.akarazhev.cryptoscout.Constants.AMQP.ROUTING_KEY_CRYPTO_BYBIT;
+import static com.github.akarazhev.cryptoscout.Constants.AMQP.ROUTING_KEY_METRICS_BYBIT;
+import static com.github.akarazhev.cryptoscout.Constants.AMQP.ROUTING_KEY_METRICS_CMC;
+import static com.github.akarazhev.cryptoscout.Constants.AMQP.X_DEAD_LETTER_EXCHANGE;
+import static com.github.akarazhev.cryptoscout.Constants.AMQP.X_DEAD_LETTER_EXCHANGE_VALUE;
+import static com.github.akarazhev.cryptoscout.Constants.AMQP.X_DEAD_LETTER_ROUTING_KEY;
 
 @Configuration
 class AmqpConfig {
@@ -66,6 +69,30 @@ class AmqpConfig {
     @Qualifier("resultsExchange")
     public TopicExchange resultsTopicExchange(@Value("${amqp.exchange.results}") final String name) {
         return ExchangeBuilder.topicExchange(name).durable(true).build();
+    }
+
+    @Bean
+    @Qualifier("metricsExchange")
+    public TopicExchange metricsExchange(@Value("${amqp.exchange.metrics}") final String name) {
+        return ExchangeBuilder.topicExchange(name)
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    @Qualifier("cryptoExchange")
+    public TopicExchange cryptoExchange(@Value("${amqp.exchange.crypto}") final String name) {
+        return ExchangeBuilder.topicExchange(name)
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    @Qualifier("clientExchange")
+    public TopicExchange clientExchange(@Value("${amqp.exchange.client}") final String name) {
+        return ExchangeBuilder.topicExchange(name)
+                .durable(true)
+                .build();
     }
 
     @Bean
@@ -99,17 +126,92 @@ class AmqpConfig {
     }
 
     @Bean
-    public MessageHandlerMethodFactory messageHandlerMethodFactory() {
-        final var factory = new DefaultMessageHandlerMethodFactory();
-        final var jsonConverter = new MappingJackson2MessageConverter();
-        jsonConverter.getObjectMapper().registerModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES));
-        factory.setMessageConverter(jsonConverter);
-        return factory;
+    public Queue cmcFearGreedIndexQueue(@Value("${amqp.queue.cmc}") final String queueName,
+                                        @Value("${amqp.queue.dead}") final String deadLetterQueue,
+                                        @Value("${amqp.queue.ttl.ms}") final int ttlMs,
+                                        @Value("${amqp.queue.max.length}") final int maxLength) {
+        return QueueBuilder.durable(queueName)
+                .withArgument(X_DEAD_LETTER_EXCHANGE, X_DEAD_LETTER_EXCHANGE_VALUE)
+                .withArgument(X_DEAD_LETTER_ROUTING_KEY, deadLetterQueue)
+                .ttl(ttlMs)
+                .maxLength(maxLength)
+                .build();
     }
 
     @Bean
-    public RabbitListenerConfigurer rabbitListenerConfigurer(final MessageHandlerMethodFactory messageHandlerMethodFactory) {
-        return (c) -> c.setMessageHandlerMethodFactory(messageHandlerMethodFactory);
+    public Queue bybitLaunchPoolQueue(@Value("${amqp.queue.bybit}") final String queueName,
+                                      @Value("${amqp.queue.dead}") final String deadLetterQueue,
+                                      @Value("${amqp.queue.ttl.ms}") final int ttlMs,
+                                      @Value("${amqp.queue.max.length}") final int maxLength) {
+        return QueueBuilder.durable(queueName)
+                .withArgument(X_DEAD_LETTER_EXCHANGE, X_DEAD_LETTER_EXCHANGE_VALUE)
+                .withArgument(X_DEAD_LETTER_ROUTING_KEY, deadLetterQueue)
+                .ttl(ttlMs)
+                .maxLength(maxLength)
+                .build();
+    }
+
+    @Bean
+    public Queue cryptoBybitQueue(@Value("${amqp.stream.crypto_bybit}") final String streamName) {
+        return QueueBuilder.durable(streamName)
+                .withArgument("x-queue-type", "stream")
+                .withArgument("x-max-length-bytes", 2_000_000_000) // 2GB max size
+                .withArgument("x-stream-max-segment-size-bytes", 100_000_000) // 100MB segments
+                .build();
+    }
+
+    @Bean
+    public Queue clientQueue(@Value("${amqp.queue.client}") final String queueName,
+                             @Value("${amqp.queue.ttl.ms}") final int ttlMs,
+                             @Value("${amqp.queue.max.length}") final int maxLength) {
+        return QueueBuilder.durable(queueName)
+                .ttl(ttlMs)
+                .maxLength(maxLength)
+                .build();
+    }
+
+    @Bean
+    public Queue metricsDeadLetterQueue(@Value("${amqp.queue.dead}") final String queueName) {
+        return QueueBuilder.durable(queueName).build();
+    }
+
+    @Bean
+    public Binding cmcFearGreedIndexBinding(final Queue cmcFearGreedIndexQueue,
+                                           @Qualifier("metricsExchange") final TopicExchange metricsExchange) {
+        return BindingBuilder.bind(cmcFearGreedIndexQueue)
+                .to(metricsExchange)
+                .with(ROUTING_KEY_METRICS_CMC);
+    }
+
+    @Bean
+    public Binding bybitLaunchPoolBinding(final Queue bybitLaunchPoolQueue,
+                                         @Qualifier("metricsExchange") final TopicExchange metricsExchange) {
+        return BindingBuilder.bind(bybitLaunchPoolQueue)
+                .to(metricsExchange)
+                .with(ROUTING_KEY_METRICS_BYBIT);
+    }
+
+    @Bean
+    public Binding cryptoBybitBinding(final Queue cryptoBybitQueue,
+                                     @Qualifier("cryptoExchange") final TopicExchange cryptoExchange) {
+        return BindingBuilder.bind(cryptoBybitQueue)
+                .to(cryptoExchange)
+                .with(ROUTING_KEY_CRYPTO_BYBIT);
+    }
+
+    @Bean
+    public Binding clientBinding(final Queue clientQueue,
+                               @Qualifier("clientExchange") final TopicExchange clientExchange) {
+        return BindingBuilder.bind(clientQueue)
+                .to(clientExchange)
+                .with(ROUTING_KEY_CLIENT);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final var rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemplate;
     }
 
     @Bean
