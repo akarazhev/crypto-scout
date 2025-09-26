0ad2a52135266d0bd1da2242f2a17ae57e73b7fb
Andrey Karazhev <andrey.karazhev@specific-group.com>
2025-09-26 21:41:30 +0300

    Added constants

---
+++ crypto-scout-client/src/main/java/com/github/akarazhev/cryptoscout/Constants.java
@@ -0,0 +1,49 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Andrey Karazhev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.akarazhev.cryptoscout;
+
+public final class Constants {
+
+    private Constants() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static final class AMQP {
+        private AMQP() {
+            throw new UnsupportedOperationException();
+        }
+        // Dead letter
+        public static final String X_DEAD_LETTER_ROUTING_KEY = "x-dead-letter-routing-key";
+        public static final String X_DEAD_LETTER_EXCHANGE = "x-dead-letter-exchange";
+        public static final String X_DEAD_LETTER_EXCHANGE_VALUE = "";
+        // Routing keys
+        public static final String ROUTING_KEY_METRICS_CMC = "metrics.cmc";
+        public static final String ROUTING_KEY_METRICS_BYBIT = "metrics.bybit";
+        public static final String ROUTING_KEY_CRYPTO_BYBIT = "crypto.bybit";
+        public static final String ROUTING_KEY_CLIENT = "client";
+        public static final String ROUTING_KEY_COLLECTOR = "collector";
+        public static final String ROUTING_KEY_TELEGRAM = "telegram";
+    }
+}
