f999ef04188398e58d70267cba0601a924431e5a
Andrey Karazhev <andrey.karazhev@specific-group.com>
2025-10-02 10:32:08 +0300

    Delete spring code.

--- crypto-scout-collector/src/main/java/com/github/akarazhev/cryptoscout/bybit/BybitLplRepository.java
+++
@@ -1,6 +0,0 @@
-package com.github.akarazhev.cryptoscout.bybit;
-
-import org.springframework.data.repository.CrudRepository;
-
-interface BybitLplRepository extends CrudRepository<BybitLpl, Long> {
-}
--- crypto-scout-collector/src/main/java/com/github/akarazhev/cryptoscout/cmc/CmcFgiRepository.java
+++
@@ -1,6 +0,0 @@
-package com.github.akarazhev.cryptoscout.cmc;
-
-import org.springframework.data.repository.CrudRepository;
-
-interface CmcFgiRepository extends CrudRepository<CmcFgi, Long> {
-}
